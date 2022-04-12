7701058e1069e1d2155a495734f011b36cf9e16d
RobbyRaffly <robbyaraffly12@gmail.com>
2022-04-12 07:53:53 +0100

    inisial commit2

--- app/src/main/java/com/adl/sharedpreference/MainActivity.kt
+++ app/src/main/java/com/adl/sharedpreference/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.adl.sharedpreference
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPreference = getSharedPreferences("login_setting", MODE_PRIVATE)
+        val username = sharedPreference.getString("username","")
+        val Checked = sharedPreference.getBoolean("checbox", false)
+
+        Username.setText(username)
+        CheckBox.setChecked(Checked)
+
+        btnLogIn.setOnClickListener{
+            var editor = sharedPreference.edit()
+            if(CheckBox.isChecked){
+                editor.putString("username",Username.toString())
+                editor.putBoolean("checkbox",true)
+                editor.commit()
+            }else{
+                editor.remove("username")
+                editor.commit()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/adl/sharedpreference/Settings.kt
@@ -0,0 +1,17 @@
+package com.adl.sharedpreference
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_settings.*
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+//        var nilai:Int=0
+//        seekBar.setOnSeekBarChangeListener{
+//
+//        }
+    }
+}
